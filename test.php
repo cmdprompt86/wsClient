2351b4ad8ef7ae8e47d3960097c36ed88110d5eb
cmdprompt86 <cmdprompt86@gmail.com>
2022-04-27 01:13:16 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,15 @@
 <?php
+require_once 'wsClient.php';
+
 $cookie='';// your cookie from keepers.mobi
  $headers[]="Cookie: _glc=$cookie";
 
-$keep=new webSocket('wss://wrap.keepers.mobi:444/socket.io/?EIO=3&transport=websocket', $headers, 30);
-if(!empty($keep->errstr)){
+$keep=new webSocket(
+     'wss://wrap.keepers.mobi:444/socket.io/?EIO=3&transport=websocket', //url
+     $headers, //additional http headers
+     30);//socket timeout
+
+if(!empty($keep->errstr)){//check for errors
  echo $keep->errstr;
  return false;
 }
